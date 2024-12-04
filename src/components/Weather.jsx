075e3fac9434bb1cc536df741d30865bbead7627
Emmanuel Egomson <fegomson@gmail.com>
2024-12-04 12:57:26 -0600

    Creating the JSX for weather widget

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,28 @@
+import { FaLocationDot, FaMagnifyingGlass } from "react-icons/fa6";
+import { AiFillSun } from "react-icons/ai";
+
+
 import "../assets/styles/Weather.css";
 
 const Weather = () => {
   return (
-    <div className="weather">Weather</div>
-  )
-}
-export default Weather
+    <div className="weather">
+      <div className="search">
+        <div className="search-top">
+          <FaLocationDot />
+          <div className="location">Dallas, Texas</div>
+        </div>
+        <div className="search-location">
+          <input type="text" placeholder="Enter location" />
+          <FaMagnifyingGlass />
+        </div>
+      </div>
+      <div className="weather-data">
+        <AiFillSun />
+        <div className="weather-type">Clear</div>
+        <div className="temp"> 28°</div>
+      </div>
+    </div>
+  );
+};
+export default Weather;
